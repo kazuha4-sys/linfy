4c74e984343b74ac47d88cddb409da178d44f843
kazuha4-sys <cleuzadasilvadossantos@gmail.com>
2025-04-16 11:27:53 -0300

    atualização nas pastas

--- back-end/curso/e/user/redirecionamento.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Função para redirecionar para a página de login
-function validaUrl($url, $url_permetidas): bool {
-    return array_key_exists(key: $url, array: $url_permetidas);
-}
-
-?>
--- back-end/login_sistem/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require ("config_db.php");
-session_start();
-session_destroy();
-header(header: "Location: pagina_cursos.html");
-?>
